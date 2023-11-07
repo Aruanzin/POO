467c9d9a5df007439cafb3adb185b95ad382e3bf
sjohnatas7 <johnatas@usp.br>
2023-11-07 13:06:07 -0300

    resolvendo merge

--- source/Modelo/Caveira.java
+++ source/Modelo/Caveira.java
@@ -1,26 +1,6 @@
 package Modelo;
 
-<<<<<<< HEAD
 import Controler.Fase;
-import interfaces.IterageComHeroi;
-import interfaces.Monstro;
-
-public class Caveira extends Personagem implements IterageComHeroi, Monstro {
-
-	private static final long serialVersionUID = 6516657816035787688L;
-
-	public Caveira() {
-		super(0, 13);
-		this.bTransponivel = false;
-	}
-
-	public boolean interageHeroi(Hero hero, Fase umaFase) {
-		return true;
-	}
-
-=======
-import java.util.ArrayList;
-
 import interfaces.IterageComHeroi;
 
 public class Caveira extends Personagem implements IterageComHeroi{
@@ -30,10 +10,9 @@ public class Caveira extends Personagem implements IterageComHeroi{
         super(0,13);
         super.bTransponivel = false;
     }
-    public boolean interageHeroi(Hero hero,ArrayList<Personagem> umaFase) {
+    public boolean interageHeroi(Hero hero,Fase umaFase) {
     	hero.setNumeroVidasRestantes(-1);
-	    umaFase.remove(this);
+	    umaFase.removePersonagem(this);
 	    return true;
     }
->>>>>>> 4fc98d404d595275a309b6496e5ea33ed9141fc9
 }
